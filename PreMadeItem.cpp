039497fdf051360a76b9014f48595f89c645c507
Arjun <arjungathwala@ucsb.edu>
2020-02-27 10:11:36 -0800

    Add files via upload

--- PreMadeItem.cpp
+++ PreMadeItem.cpp
@@ -1,14 +1,29 @@
-#include<iostream>
-#include"PreMadeItem.h"
+#include "PreMadeItem.h"
+#include "IceCreamItem.h"
 
-PreMadeItem::PreMadeItem(std::string name, std::string size):IceCreamItem(size)
-{
+#include <string>
+#include <iomanip>
+#include <sstream>
+#include <utility>
+#include <vector>
+
+using namespace std;
+
+PreMadeItem::PreMadeItem(string name, string size): IceCreamItem(size) {
 this->name = name;
-if(size == "small"){
-    IceCreamItem::price = 4.00;
-}else if(size == "medium"){
-    IceCreamItem::price = 6.00;
-}else if(size == "large"){
-    IceCreamItem::price = 7.50;
 }
+
+PreMadeItem::~PreMadeItem() {}
+
+string PreMadeItem::composeItem() {
+string item = "Pre-made Size: " + IceCreamItem::composeItem() + "\n";
+item += "Pre-made Item: " + name + "\n";
+stringstream stream;
+stream << fixed << setprecision(2) << getPrice() << endl;
+item += ("Price: $" + stream.str());
+return item;
+}
+
+double PreMadeItem::getPrice() {
+return IceCreamItem::getPrice() + 1;
 }
--- PreMadeItem.h
+++ PreMadeItem.h
@@ -1,39 +1,18 @@
 #ifndef PREMADEITEM_H
 #define PREMADEITEM_H
 
-#include <utility>
-#include <iomanip>
-#include <sstream>
-#include <vector>
 #include "IceCreamItem.h"
-#include <iostream>  
-#include <sstream>  
+#include <string>
 
-class PreMadeItem : public IceCreamItem
-{
+class PreMadeItem:public IceCreamItem {
 public:
-    PreMadeItem(std::string name, std::string size);
-    virtual ~PreMadeItem(){}
-
-    double getPrice(){return price;}
-
-    std::string composeItem()
-    {
-      std::stringstream ss;
-      ss<<std::fixed<<std::setprecision(3)<<getPrice();
-      std::string s;
-      ss>>s;
-      std::string prestr;
-      prestr = "Pre-made Size: "+PreMadeItem::size+"\n";
-      prestr += "Pre-made Item: "+PreMadeItem::name+"\n";
-      prestr += "Price: $" + s+"\n";
-      prestr += "-----\n";
-      prestr += "Total: $" + s+"\n";
-      return prestr;
-    }
-
-protected :
+PreMadeItem(std::string name, std::string size);
+virtual ~PreMadeItem();
+virtual std::string composeItem();
+virtual double getPrice();
+private:
 std::string name;
 };
+
 
 #endif
